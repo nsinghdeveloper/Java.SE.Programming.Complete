5fd0afac0d349c2e63a817d93797c98364d37f9a
Shackluryz <alem.shackuryz@gmail.com>
2021-03-14 20:48:49 -0300

    Classe Shop
    Implementando o objeto da classe Product, utilizando os métodos  para inicializar os atributos e exibi-los no console

--- ProductManangement/src/labs/pm/app/Shop.java
+++ ProductManangement/src/labs/pm/app/Shop.java
@@ -5,6 +5,9 @@
  */
 package labs.pm.app;
 
+import java.math.BigDecimal;
+import labs.pm.data.Product;
+
 /**
  *
  * @author Alem
@@ -15,7 +18,12 @@ public class Shop {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Product p1 = new Product();
+        p1.setId(101);
+        p1.setName("Tea");
+        p1.setPrice(BigDecimal.valueOf(1.99));
+        
+        System.out.println(p1.getId()+" "+p1.getName()+" "+p1.getPrice());
     }
     
 }
