3303a846945a0803f48430869d12f1fef617d015
Shackluryz <alem.shackuryz@gmail.com>
2021-03-14 21:52:02 -0300

    Class Project
    Ajustes na documentação

--- ProductManangement/src/labs/pm/data/Product.java
+++ ProductManangement/src/labs/pm/data/Product.java
@@ -20,12 +20,26 @@ import java.math.BigDecimal;
 import static java.math.RoundingMode.HALF_UP;
 
 /**
+ * {@code Product} class represents proprieties and behaviours of product
+ * objects in the Product Management System.
+ * <br>
+ * Each product has an id, name, and price
+ * <br>
+ * Each product can have a discount, calculated based on a
+ * {@link DISCOUNT_RATE discount rate}
  *
+ * @version 4.0
  * @author Alem
  */
 public class Product {
 
-    public static final BigDecimal DISCOUNT_RATE=BigDecimal.valueOf(0.1);
+    /**
+     * A constant that defines a {@link java.math.BigDecimal BigDecimal} value
+     * of the discount rate
+     * <br>
+     * Discount rate is 10%
+     */
+    public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
     private int id;
     private String name;
     private BigDecimal price;
@@ -53,8 +67,14 @@ public class Product {
     public void setPrice(final BigDecimal price) {
         this.price = price;
     }
-    
-    public BigDecimal getDiscount(){
+
+    /**
+     * Calculates discount based on a product price and
+     * {@link  DISCOUNT_RATE discount rate}
+     *
+     * @return a {@link java.math.BigDecimal BigDecimal} value of the discount
+     */
+    public BigDecimal getDiscount() {
         return price.multiply(DISCOUNT_RATE).setScale(2, HALF_UP);
-    }    
+    }
 }
