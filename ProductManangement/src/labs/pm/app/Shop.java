193525a12336a89ff23ae5b7f1b9683ced6c67ae
Shackluryz <alem.shackuryz@gmail.com>
2021-03-14 21:12:47 -0300

    Class Shop
    Ajustes na documentação presente na classe

--- ProductManangement/src/labs/pm/app/Shop.java
+++ ProductManangement/src/labs/pm/app/Shop.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2021 Alem
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package labs.pm.app;
 
@@ -9,7 +20,8 @@ import java.math.BigDecimal;
 import labs.pm.data.Product;
 
 /**
- *
+ * {@code Shop} class represents an aplication that manages Products
+ * @version 4.0
  * @author Alem
  */
 public class Shop {
@@ -23,7 +35,7 @@ public class Shop {
         p1.setName("Tea");
         p1.setPrice(BigDecimal.valueOf(1.99));
         
-        System.out.println(p1.getId()+" "+p1.getName()+" "+p1.getPrice());
+        System.out.println(p1.getId()+" "+p1.getName()+" "+p1.getPrice()+" "+p1.getDiscount());
     }
     
 }
