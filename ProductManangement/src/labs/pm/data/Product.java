0e01571bdb4a3a3ecf9884139be92945aba601c7
Shackluryz <alem.shackuryz@gmail.com>
2021-03-23 17:24:48 -0300

    Construct Methods
    Implementação e teste dos metodos construtores da classe Produtos

--- ProductManangement/src/labs/pm/data/Product.java
+++ ProductManangement/src/labs/pm/data/Product.java
@@ -18,6 +18,7 @@ package labs.pm.data;
 
 import java.math.BigDecimal;
 import static java.math.RoundingMode.HALF_UP;
+import static labs.pm.data.Rating.*;
 
 /**
  * {@code Product} class represents proprieties and behaviours of product
@@ -44,6 +45,21 @@ public class Product {
     private String name;
     private BigDecimal price;
     private Rating rating;
+    
+    public Product(){
+        this(0, "no name", BigDecimal.ZERO);
+    }
+
+    public Product(int id, String name, BigDecimal price, Rating rating) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+    
+    public Product(int id, String name, BigDecimal price) {
+        this(id, name, price, NOT_RATED);
+    }
 
     public int getId() {
         return id;
